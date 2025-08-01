6df2f08cb35a7d94bd6aa39b1167c52dcfa8db01
bohodays <bohodays0316@gmail.com>
2025-08-01 16:30:38 +0900

    design: SimpleCount 컴포넌트 퍼블리싱

--- src/components/SimpleCount.js
+++ src/components/SimpleCount.js
@@ -1,5 +1,12 @@
 const SimpleCount = () => {
-  return `<div>SimpleCount</div>`;
+  return `
+  <div class="container">
+    <h1>count</h1>
+    <div class="buttons">
+      <button>+</button>
+      <button>-</button>
+    </div>
+  </div>`;
 };
 
 export default SimpleCount;
